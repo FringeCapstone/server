e79c057d644d042e691f2fa71e0fbe3d5981e933
mmendiola2824 <mendiola.matthew.91@gmail.com>
2024-04-19 13:57:53 -0500

    updates to API

--- controllers/journalsController.js
+++ controllers/journalsController.js
@@ -4,8 +4,17 @@ exports.getJournals = async (req, res) => {
   Journals.fetchAllJournals(req.params.id)
       .then(journals => res.json(journals))
 }
+exports.getDocs = async (req, res) => {
+  Journals.fetchAllDoc()
+      .then(journals => res.json(journals))
+}
 
 exports.getAllJournals = async (req, res) => {
   Journals.fetchAll()
       .then(journals => res.json(journals))
+}
+
+exports.addJournal = async (req, res) => {
+  Journals.addJournal(req.params.id, req.body)
+      .then(journal => res.json(journal))
 }
--- routes/journalsRoutes.js
+++ routes/journalsRoutes.js
@@ -2,8 +2,10 @@ const express = require('express');
 const router = express.Router();
 const journalsController = require('../controllers/journalsController');
 
-router.get('/showJournals', journalsController.getAllJournals);
+router.get('/showJournals', journalsController.getAllJournals);// wont have implemented
+router.get('/showdocs', journalsController.getDocs);// wont have implemented
 router.get('/showJournals/:id', journalsController.getJournals);
+router.post('/new', journalsController.addJournal);
 
 
 exports.routes = router;
